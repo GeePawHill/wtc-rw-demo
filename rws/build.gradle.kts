4103ba9ecf373ff61b672696a6f071f628f85eb4
GeePaw Hill <geepawhill@geepawhill.org>
2021-05-24 13:06:55 -0400

    First attempt at first project.

---
+++ rws/build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "org.geepawhill"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    application
+    java
+    kotlin("jvm") version "1.4.10"
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClassName = "org.geepawhill.rws.AppKt"
+}
+
+kotlin {
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "14"
+        }
+    }
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+    testImplementation("org.assertj:assertj-core:3.11.1")
+}
