c3f9dff8640b810d208ee0a484a75ff309755abb
GeePaw Hill <geepawhill@geepawhill.org>
2021-05-24 13:53:54 -0400

    Utility project rwu added.

--- rwu/build.gradle.kts
+++ rwu/build.gradle.kts
@@ -4,14 +4,10 @@ group = "org.geepawhill"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    application
-    java
-    kotlin("jvm") version "1.5.10"
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
 }
 
 repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
     jcenter()
 }
 
@@ -19,11 +15,6 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
-
-application {
-    mainClass.set("org.geepawhill.rws.AppKt")
-}
-
 kotlin {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
@@ -34,10 +25,8 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
     testImplementation("org.assertj:assertj-core:3.11.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "wtc-rw-demo"
 include("rws")
+include("rwu")
